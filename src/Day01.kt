595a82115b48466b9baf0ce6a66e6be6df39413c
Vidhem <vidhem.chhabra@gmail.com>
2021-12-09 18:38:51 +0530

    Day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,47 @@
+import java.util.*
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var increases = 0
+        var last = input[0].toInt()
+
+        input.map { it.toInt() }.forEach {
+            if (it > last) {
+                increases++;
+            }
+            last = it
+        }
+
+        return increases
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val window = LinkedList<Int>()
+        val depths = input.map { it.toInt() }
+
+        for (i in 0..2) {
+            window.add(depths[i])
+        }
+
+        var sum = window.reduce { acc, i -> acc + i }
+        var increases = 0
+        for (i in 3 until depths.size) {
+            val newSum = sum - window.peek() + depths[i]
+            if (newSum > sum) {
+                increases++
+            }
+
+            sum = newSum
+            window.remove()
+            window.add(depths[i])
+        }
+
+        return increases
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
