9f0775da20188033cc21958b37e0662f2b34cc6b
Vidhem <vidhem.chhabra@gmail.com>
2021-12-10 16:31:59 +0530

    Day 3 part 1

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,8 +1,23 @@
-import java.util.*
-
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val bitCounts: ArrayList<HashMap<String, Int>> = ArrayList()
+        repeat(input[0].split("").size-2) {
+            bitCounts.add(HashMap())
+        }
+
+        input.forEach {
+            it.split("").drop(1).dropLast(1).forEachIndexed { index, bit ->
+                bitCounts[index][bit] = bitCounts[index].getOrDefault(bit, 0) + 1
+            }
+        }
+
+        val gamma = bitCounts.fold("") { acc, counts -> acc + counts.maxByOrNull { it.value }!!.key }
+            .toInt(2)
+
+        val epsilon = bitCounts.fold("") { acc, counts -> acc + counts.minByOrNull { it.value }!!.key }
+            .toInt(2)
+
+        return gamma * epsilon
     }
 
     fun part2(input: List<String>): Int {
@@ -11,7 +26,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day03_test")
-    check(part2(testInput) == 5)
+    check(part1(testInput) == 198)
 
     val input = readInput("Day03")
     println(part1(input))
