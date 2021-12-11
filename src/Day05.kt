d490ea8e5221ae2027b3267f74a90c4719b6504d
Vidhem <vidhem.chhabra@gmail.com>
2021-12-12 00:30:03 +0530

    Day 5 part 2

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,3 +1,5 @@
+
+import kotlin.math.abs
 import kotlin.math.max
 import kotlin.math.min
 
@@ -31,12 +33,45 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val grid = MutableList(1000) { MutableList(1000) { 0 } }
+
+        input.forEach { line ->
+            val (p1, p2) = line.split(" -> ")
+            val (x1, y1) = p1.split(",").map { it.toInt() }
+            val (x2, y2) = p2.split(",").map { it.toInt() }
+
+            if (x1 != x2 && y1 != y2) {
+                val xdir = if (x2 > x1) 1 else -1
+                val ydir = if (y2 > y1) 1 else -1
+
+                var x = x1
+                var y = y1
+                repeat(abs(x2-x1) + 1) {
+                    grid[x][y]++
+                    x += xdir
+                    y += ydir
+                }
+            }
+
+            if (x1 == x2) {
+                for (index in min(y1, y2)..max(y1, y2)) {
+                    grid[x1][index]++
+                }
+            }
+
+            if (y1 == y2) {
+                for (index in min(x1, x2)..max(x1, x2)) {
+                    grid[index][y1]++
+                }
+            }
+        }
+
+        return grid.sumOf { it.filter { el -> el > 1 }.size }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day05_test")
-    check(part1(testInput) == 5)
+    check(part2(testInput) == 12)
 
     val input = readInput("Day05")
     println(part1(input))
