93a16740b11a1eb074179261147e983c623ab7f2
Vidhem <vidhem.chhabra@gmail.com>
2021-12-12 02:31:53 +0530

    Day 7 solutions

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,15 +1,25 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val positions = input[0].split(",").map { it.toInt() }
+        val minPos = positions.minOrNull() ?: 0
+        val maxPos = positions.maxOrNull() ?: 0
+
+        return (minPos..maxPos).minOf { positions.sumOf { pos -> abs(pos - it) } }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val positions = input[0].split(",").map { it.toInt() }
+        val minPos = positions.minOrNull() ?: 0
+        val maxPos = positions.maxOrNull() ?: 0
+
+        return (minPos..maxPos).minOf { positions.sumOf { pos -> (abs(pos - it) * (abs(pos - it) + 1))/2 } }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day07_test")
-    check(part2(testInput) == 5)
+    check(part2(testInput) == 168)
 
     val input = readInput("Day07")
     println(part1(input))
