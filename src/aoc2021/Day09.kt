71a376ef3e12a9e1bb0b34e83c53519917db8ff0
Vidhem <vidhem.chhabra@gmail.com>
2021-12-13 01:09:47 +0530

    Day 9 part 2

--- src/aoc2021/Day09.kt
+++ src/aoc2021/Day09.kt
@@ -1,9 +1,13 @@
 package aoc2021
 
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashSet
+
 fun main() {
 
-    fun getLowPoints(heightMap: List<List<Int>>): List<Int> {
-        val lowPoints: MutableList<Int> = ArrayList()
+    fun getLowPoints(heightMap: List<List<Int>>): List<Pair<Int,Int>> {
+        val lowPoints: MutableList<Pair<Int,Int>> = ArrayList()
         val totalW = heightMap[0].size
         val totalH = heightMap.size
 
@@ -24,7 +28,7 @@ fun main() {
                 }
 
                 if (comparisonPoints.all { it > height }) {
-                    lowPoints.add(height)
+                    lowPoints.add(Pair(row, column))
                 }
             }
         }
@@ -34,15 +38,47 @@ fun main() {
 
     fun part1(input: List<String>): Int {
         val heightMap = input.map { it.toCharArray().map { digit -> digit.digitToInt() } }
-        return getLowPoints(heightMap).sumOf { it + 1 }
+        return getLowPoints(heightMap).sumOf { heightMap[it.first][it.second] + 1 }
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        val heightMap = input.map { it.toCharArray().map { digit -> digit.digitToInt() } }
+        val totalW = heightMap[0].size
+        val totalH = heightMap.size
+        val lowPoints = getLowPoints(heightMap)
+
+        val basinSizes = PriorityQueue<Int>(compareByDescending { it })
+
+        for (lowPoint in lowPoints) {
+            val queue: Queue<Pair<Int,Int>> = LinkedList()
+            queue.add(lowPoint)
+
+            val visited: MutableSet<Pair<Int,Int>> = HashSet()
+            while (queue.isNotEmpty()) {
+                val (row, column) = queue.remove()
+                visited.add(Pair(row, column))
+
+                if (row > 0 && heightMap[row-1][column] != 9 && !visited.contains(Pair(row-1, column))) {
+                    queue.add(Pair(row-1, column))
+                }
+                if (row + 1 < totalH && heightMap[row+1][column] != 9 && !visited.contains(Pair(row+1, column))) {
+                    queue.add(Pair(row+1, column))
+                }
+                if (column > 0 && heightMap[row][column-1] != 9 && !visited.contains(Pair(row, column-1))) {
+                    queue.add(Pair(row, column-1))
+                }
+                if (column + 1 < totalW && heightMap[row][column+1] != 9 && !visited.contains(Pair(row, column+1))) {
+                    queue.add(Pair(row, column+1))
+                }
+            }
+            basinSizes.add(visited.size)
+        }
+
+        return basinSizes.remove() * basinSizes.remove() * basinSizes.remove()
     }
 
     val testInput = readInput("Day09_test")
-    check(part1(testInput) == 15)
+    check(part2(testInput) == 1134)
 
     val input = readInput("Day09")
     println(part1(input))
