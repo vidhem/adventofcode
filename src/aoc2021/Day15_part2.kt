50e547c63973cf5cf52949a40bf2bc6fdc510488
Vidhem <vidhem.chhabra@gmail.com>
2021-12-16 00:06:42 +0530

    Day 15 part 2

--- src/aoc2021/Day15_part2.kt
+++ src/aoc2021/Day15_part2.kt
@@ -1,23 +1,13 @@
 package aoc2021
 
+import java.util.*
+
 fun main() {
 
-    fun findPath(input: List<List<Int>>, memoize: MutableList<MutableList<Int>>, x: Int = 0, y: Int = 0): Int {
-        if (memoize[y][x] != -1) {
-            return memoize[y][x]
+    data class Node(val point: Pair<Int, Int>, val distance: Int = Int.MAX_VALUE) : Comparable<Node> {
+        override fun compareTo(other: Node): Int {
+            return this.distance - other.distance
         }
-
-        if (x < input[0].size - 1 && y < input.size - 1) {
-            memoize[y][x] = input[y][x] + minOf(findPath(input, memoize, x+1, y), findPath(input, memoize, x, y+1))
-        } else if (x < input[0].size - 1) {
-            memoize[y][x] = input[y][x] + findPath(input, memoize, x+1, y)
-        } else if (y < input.size - 1) {
-            memoize[y][x] = input[y][x] + findPath(input, memoize, x, y+1)
-        } else {
-            memoize[y][x] = input[y][x]
-        }
-
-        return memoize[y][x]
     }
 
     fun part2(input: List<String>): Int {
@@ -42,8 +32,46 @@ fun main() {
             }
         }
 
-        val memoize = MutableList(5 * grid.size) { MutableList(5 * grid[0].size) { -1 } }
-        return findPath(grid, memoize) - grid[0][0]
+        val visited: MutableSet<Pair<Int, Int>> = HashSet()
+        val distances = MutableList(grid.size) { MutableList(grid[0].size) { Int.MAX_VALUE } }
+
+        val traversal = PriorityQueue<Node>()
+        traversal.add(Node(Pair(0, 0), 0))
+
+        while (visited.size < grid.size * grid[0].size) {
+            val node = traversal.remove()
+            if (visited.contains(node.point)) {
+                continue
+            }
+
+            val (x, y) = node.point
+
+            distances[y][x] = node.distance
+            visited.add(node.point)
+
+            val edges = HashSet<Pair<Int, Int>>()
+            if (x > 0) {
+                edges.add(Pair(x-1, y))
+            }
+            if (y > 0) {
+                edges.add(Pair(x, y-1))
+            }
+            if (x < grid[0].size - 1) {
+                edges.add(Pair(x+1, y))
+            }
+            if (y < grid.size - 1) {
+                edges.add(Pair(x, y+1))
+            }
+
+            edges.forEach {
+                if (distances[it.second][it.first] > grid[it.second][it.first] + node.distance) {
+                    distances[it.second][it.first] = grid[it.second][it.first] + node.distance
+                    traversal.add(Node(Pair(it.first, it.second), distances[it.second][it.first]))
+                }
+            }
+        }
+
+        return distances.last().last()
     }
 
     val testInput = readInput("Day15_test")
