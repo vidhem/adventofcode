b192e0a69091abd28f0c28145fbb7bdb9ea7b851
Vidhem <vidhem.chhabra@gmail.com>
2021-12-19 23:39:42 +0530

    Refactor Day 17

--- src/aoc2021/Day17.kt
+++ src/aoc2021/Day17.kt
@@ -3,59 +3,13 @@ package aoc2021
 import kotlin.math.max
 
 fun main() {
-    fun part1(input: String): Int {
+
+    fun getPossibleVels(input: String): Set<Pair<Int, Int>> {
         val targetParts = input.substring("target area: ".length).split(", ")
         val ranges = targetParts.map {
             it.split("=")[1]
                 .split("..")
                 .map { it.toInt() }
-        }
-
-        val possibleXVels = (1..ranges[0][1]).filter {
-            val dist = it * (it + 1) / 2
-            ranges[0][0] <= dist && dist <= ranges[0][1]
-        }
-
-        val vels = HashSet<Pair<Int, Int>>()
-        for (x in possibleXVels) {
-            for (y in 1..1000) {
-                var vel = Pair(x, y)
-                var pos = Pair(0, 0)
-
-                while (true) {
-                    if (pos.first >= ranges[0][0]
-                        && pos.first <= ranges[0][1]
-                        && pos.second >= ranges[1][0]
-                        && pos.second <= ranges[1][1]) {
-                        vels.add(Pair(x, y))
-                        break
-                    }
-
-                    if (pos.second < ranges[1][0] || x > ranges[0][1]) {
-                        break
-                    }
-
-                    pos = Pair(pos.first + vel.first, pos.second + vel.second)
-                    vel = Pair(max(vel.first - 1, 0), vel.second - 1)
-                }
-            }
-        }
-
-        val maxYVel = vels.maxOf { it.second }
-        return maxYVel * (maxYVel + 1) / 2
-    }
-
-    fun part2(input: String): Int {
-        val targetParts = input.substring("target area: ".length).split(", ")
-        val ranges = targetParts.map {
-            it.split("=")[1]
-                .split("..")
-                .map { it.toInt() }
-        }
-
-        val possibleXVels = (1..ranges[0][1]).filter {
-            val dist = it * (it + 1) / 2
-            ranges[0][0] <= dist && dist <= ranges[0][1]
         }
 
         val vels = HashSet<Pair<Int, Int>>()
@@ -83,7 +37,16 @@ fun main() {
             }
         }
 
-        return vels.size
+        return vels.toSet()
+    }
+
+    fun part1(input: String): Int {
+        val maxYVel = getPossibleVels(input).maxOf { it.second }
+        return maxYVel * (maxYVel + 1) / 2
+    }
+
+    fun part2(input: String): Int {
+        return getPossibleVels(input).size
     }
 
     val testInput = readInput("Day17_test")[0]
